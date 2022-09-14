5f1a1dafe7f93cd6f951a80f42593aae93d70fba
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-09-14 15:26:16 +0100

    220914

---
+++ LabSheets/Lab1/NumbersMenu.java
@@ -0,0 +1,9 @@
+package Lab1;
+
+public class NumbersMenu {
+    public static void main(String[] args) {
+        int iter = 0;
+        System.out.printf("---Iteration %d---%n",(iter+1) );
+        System.out.println("Please enter any whole number: ");
+    }
+}

a113489c1192030345b909469490488086f5e709
t00229173 <Han.Lim.Oah@students.ittralee.ie>
2022-09-15 11:10:44 +0100

    220915

--- LabSheets/Lab1/HeightStats.java
+++ LabSheets/Lab1/HeightStats.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 public class HeightStats {
     public static void main(String[] args) {
         try {
-            Scanner line = new Scanner(System.in);
-            double h;
             int iter = 0;
             final int loop = 6;
-            final double maxH = 2.72;
-            final double minH = 0.5464;
+            do {
+                Scanner line = new Scanner(System.in);
+                double h;
+                double[] hArr = new double[6];
+                final double maxH = 2.72;
+                final double minH = 0.5464;
 
+                System.out.printf("Loop %d - Please enter height: %n", iter+1);
+                h = line.nextDouble();
 
-            System.out.printf("Loop %d - Please enter height: %n", iter+1);
+                while(h > maxH || h< minH){
+                    System.out.printf("Loop %d - Height value invalid !!! Please enter height: %n", iter+1);
+                    h = line.nextDouble();
+                }
+                hArr[iter] = h;
+                iter++;
 
-            h = line.nextDouble();
-            boolean neg = h < 0;
-            switch (h){
-                case > maxH:
-            }
+            } while (iter < loop);
         }
         catch (Exception e){
             throw new RuntimeException("Please enter relevant information.");

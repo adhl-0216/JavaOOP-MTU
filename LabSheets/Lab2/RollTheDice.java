22daf8c627f9145347c7dbf9048f0487e704af2c
T00229173 <Han.Lim.Oah@students.ittralee.ie>
2022-09-20 15:48:54 +0100

    220920

--- LabSheets/Lab2/RollTheDice.java
+++ LabSheets/Lab2/RollTheDice.java
@@ -8,10 +8,18 @@ public class RollTheDice {
 
         switch (response){
             case "N" -> JOptionPane.showMessageDialog(null, "Thanks for playing the game!","Farewell",JOptionPane.INFORMATION_MESSAGE);
-            case "Y" -> System.out.println("yay");
+            case "Y" -> rollLeDice();
         }
 
         System.exit(0);
 
+    }
+
+    private static void rollLeDice() {
+        int dice1 = (int) (1 + (Math.random()*6));
+        int dice2 = (int) (1 + (Math.random()*6));
+        int compRoll = dice1 + dice2;
+        System.out.println(dice1 +" "+ dice2);
+        JOptionPane.showMessageDialog(null, "The Computer rolled: " + compRoll );
     }
 }

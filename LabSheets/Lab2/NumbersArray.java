8bdf7d15439e1bf1decc11d0d50354340d074479
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-09-21 14:37:03 +0100

    220921

--- LabSheets/Lab2/NumbersArray.java
+++ LabSheets/Lab2/NumbersArray.java
@@ -1,26 +1,41 @@
 package Lab2;
 
+import javax.swing.*;
 import java.util.Arrays;
-import java.util.OptionalDouble;
-import java.util.StringTokenizer;
 
 public class NumbersArray {
     public static void main(String[] args) {
         double[] arr = new double[]{22.3, 45.6, 27.4, 56.6, 73.2, 11.5, 87.4, 23.8};
-//        System.out.println(Arrays.toString(arr));
-        double avg = 0;
-        String str = "";
-        System.out.println( largest(arr));
+        double avg;
+        String abvAvg;
+
         avg = average(arr);
-        str = aboveAverage(arr,avg);
+        abvAvg = aboveAverage(arr,avg);
+        String str = abvAvg.substring(1,abvAvg.lastIndexOf(']'));
+
+        String output = String.format("""
+                The largest value in the array is %.1f \t
+                The average value in the array is %.3f \t
+                The list of values above the average is: \n%s
+                """,largest(arr),avg,str);
+        JOptionPane.showMessageDialog(null, output);
+        System.exit(0);
     }
 
     private static String aboveAverage(double[] arr, double avg) {
-        return "";
+        arr = Arrays.stream(arr).sorted().toArray();
+        double[] sliced = new double[0];
+        for (int i = 0; i<arr.length;i++) {
+            if ((arr[i] > avg)) {
+                sliced = Arrays.copyOfRange(arr, i, arr.length);
+                break;
+            }
+        }
+        return Arrays.toString(sliced);
     }
 
     private static double average(double[] arr) {
-        return 0;
+        return Arrays.stream(arr).summaryStatistics().getAverage();
     }
 
     private static double largest(double[] arr) {

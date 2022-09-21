bb5e02395e72e48b537d6b2dd383d63ea2697a04
t00229173 <Han.Lim.Oah@students.ittralee.ie>
2022-09-21 10:58:03 +0100

    220920

--- LabSheets/Lab2/NamesArray.java
+++ LabSheets/Lab2/NamesArray.java
@@ -6,8 +6,21 @@ import java.util.Arrays;
 public class NamesArray {
     static String[] names = new String[5];
     public static void main(String[] args) {
+        double avgLen = 0;
+        String longest = "";
         populateArray();
-        System.out.println(Arrays.toString(names));
+        for (String str: names) {
+            if (longest.length() == 0) longest = str;
+            else if (longest.length() < str.length()) longest = str;
+            avgLen += str.length();
+        }
+        Arrays.sort(names);
+        avgLen = Math.round(avgLen/5);
+
+        JOptionPane.showMessageDialog(null,"The longest name is " + longest +
+                "\nAverage character per name is " + (int)avgLen +
+                "\nThe array sorted in ascending order is \n" +
+                Arrays.toString(names));
     }
     private static void populateArray() {
         for (int i = 0; i < 5; i++) {
@@ -16,6 +29,4 @@ public class NamesArray {
             names[i] = JOptionPane.showInputDialog(msg);
         }
     }
-
-
 }

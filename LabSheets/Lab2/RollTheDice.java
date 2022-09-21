773d5532d3533996ec96072efb38456da8d365ee
t00229173 <Han.Lim.Oah@students.ittralee.ie>
2022-09-21 10:14:15 +0100

    220915

--- LabSheets/Lab2/RollTheDice.java
+++ LabSheets/Lab2/RollTheDice.java
@@ -1,25 +1,56 @@
 package Lab2;
 
 import javax.swing.*;
+import java.util.Objects;
 
 public class RollTheDice {
+    static int compWs = 0 ;
+    static int playerWs = 0;
+    static int draws = 0;
+    static int gamesPlayed = 0;
     public static void main(String[] args) {
-        String response = JOptionPane.showInputDialog("Would you like to play a game of dice? (Y or N)");
 
-        switch (response){
-            case "N" -> JOptionPane.showMessageDialog(null, "Thanks for playing the game!","Farewell",JOptionPane.INFORMATION_MESSAGE);
-            case "Y" -> rollLeDice();
+        do {
+            String response = JOptionPane.showInputDialog("Would you like to play a game of dice? (Y or N)");
+
+            if (Objects.equals(response.toUpperCase() , "N")){
+                JOptionPane.showMessageDialog(null, "Thanks for playing the game!","Farewell",JOptionPane.INFORMATION_MESSAGE);
+                break;
+            } else if (Objects.equals(response.toUpperCase(), "Y")) {
+                rollLeDice();
+                gamesPlayed++;
+                JOptionPane.showMessageDialog(null,
+                        "Games played: " + gamesPlayed +
+                                "\nComputer Wins: " + compWs +
+                                "\nPlayer wins: " + playerWs +
+                                "\nDraws: "+ draws);
+
+            }
         }
+        while (true);
 
         System.exit(0);
-
     }
 
-    private static void rollLeDice() {
-        int dice1 = (int) (1 + (Math.random()*6));
-        int dice2 = (int) (1 + (Math.random()*6));
-        int compRoll = dice1 + dice2;
-        System.out.println(dice1 +" "+ dice2);
-        JOptionPane.showMessageDialog(null, "The Computer rolled: " + compRoll );
+    public static void rollLeDice() {
+        int compRoll = 0;
+        int playerRoll = 0;
+
+        for (int i = 0; i < 2; i++) {
+            int dice1 = (int) (1 + (Math.random() * 6));
+            int dice2 = (int) (1 + (Math.random() * 6));
+            if (i == 0) {
+                compRoll = dice1 + dice2;
+                JOptionPane.showMessageDialog(null, "The Computer rolled a " + compRoll + " !" +
+                        "\nPress enter to roll your dice!");
+            } else {
+                playerRoll = dice1 + dice2;
+                JOptionPane.showMessageDialog(null, "You rolled a " + playerRoll + " !");
+            }
+        }
+        if (compRoll == playerRoll) draws++;
+        else if(compRoll < playerRoll) playerWs++;
+        else compWs++;
     }
 }
+

afac06a6fba27a2494a3bbd09ee8d3408b2c624a
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-09-21 23:05:09 +0100

    220921

---
+++ LabSheets/Lab2/WeightConverter.java
@@ -0,0 +1,10 @@
+package Lab2;
+
+import javax.swing.*;
+
+public class WeightConverter {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Weight Converter");
+        JFrame.getFrames();
+    }
+}

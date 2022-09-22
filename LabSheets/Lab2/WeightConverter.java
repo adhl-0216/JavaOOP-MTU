540e99d5b883b494417b96626f4565209a42b328
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-09-22 15:41:20 +0100

    220922 unfinished

--- LabSheets/Lab2/WeightConverter.java
+++ LabSheets/Lab2/WeightConverter.java
@@ -1,10 +1,61 @@
 package Lab2;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+//create frame
 
 public class WeightConverter {
+    private static JFrame mainFrame;
+    private static JLabel statusLabel;
+    private static JPanel controlPanel;
     public static void main(String[] args) {
-        JFrame window = new JFrame("Weight Converter");
-        JFrame.getFrames();
+        double kg = 0;
+
+//        JLabel labelKilos = new JLabel(String.format( "This is equivalent to %.2fkg", kg));
+        inputField();
+        prepareGUI();
+
+
     }
+
+    private static void prepareGUI() {
+        mainFrame = new JFrame("Weight Converter");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(300, 100);
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        statusLabel = new JLabel("",JLabel.CENTER);
+
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+
+        mainFrame.add(controlPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
+    }
+
+    private static void inputField(){
+        JLabel poundsLabel = new JLabel("Pounds: ");
+        final JTextField poundsText = new JTextField(5);
+
+        poundsText.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String data = poundsText.getText();
+                statusLabel.setText(data);
+            }
+        });
+        controlPanel.add(poundsLabel);
+        controlPanel.add(poundsText);
+        mainFrame.setVisible(true);
+    }
+
 }
+

501473da7acba6f24280c792dfc6b28beefd7e64
T00229173 <Han.Lim.Oah@students.ittralee.ie>
2022-09-27 14:39:35 +0100

    220927

---
+++ LabSheets/Lab4/exercise1n2/TestComputer.java
@@ -0,0 +1,29 @@
+package Lab4.exercise1n2;
+
+import javax.swing.*;
+
+public class TestComputer {
+    public static void main(String[] args) {
+        String output = "";
+        Computer c1 = new Computer();
+
+    output += "Calling the no-argument Computer constructor. "
+            + "The first Computer object details are: \n\n"
+            + c1;
+/*    Computer c2 = new Computer("Dell","Laptop",3.25,16,550.99);
+
+    c2.setSpeed(-3.5);
+
+    output += "\n\nCalling the multi-argument Computer constructor. "
+            + "The second Computer object details are: \n\n" + c2;
+
+    Computer c3 = new Computer("Apple","Tablet",3.65,-16,-500.25);
+
+
+    output += "\n\nCalling the multi-argument Computer constructor. "
+            + "The third Computer object details are: \n\n" + c3;*/
+
+    JOptionPane.showMessageDialog(null,output,"Computer Object Data", JOptionPane.INFORMATION_MESSAGE);
+
+    System.exit(0); }
+}

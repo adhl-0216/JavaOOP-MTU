1a48eeac856a3ff84b6fd26e7392b48071195d11
T00229173 <Han.Lim.Oah@students.ittralee.ie>
2022-09-27 15:53:09 +0100

    220927

--- LabSheets/Lab5/exercise4/BankAccount.java
+++ LabSheets/Lab5/exercise4/BankAccount.java
@@ -1,4 +1,48 @@
 package Lab5.exercise4;
 
 public class BankAccount {
+    private String owner;
+    private int member;
+    private static double interestRate;
+
+    public BankAccount(){
+        setOwner("Owner not Available.");
+        setMember(0);
+        setInterestRate(0);
+    }
+
+    public BankAccount
+            (
+                String owner,
+                int member,
+                double interestRate
+            ){
+        setOwner(owner);
+        setMember(member);
+        setInterestRate(interestRate);
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setMember(int member) {
+        this.member = member;
+    }
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public int getMember() {
+        return member;
+    }
+
+    public static double getInterestRate() {
+        return interestRate;
+    }
 }

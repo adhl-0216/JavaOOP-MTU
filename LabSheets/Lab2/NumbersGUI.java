e91184a4dabae7be79860c032b16ce088ec7474e
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-09-27 17:53:10 +0100

    220928

--- LabSheets/Lab2/NumbersGUI.java
+++ LabSheets/Lab2/NumbersGUI.java
@@ -1,18 +1,50 @@
 package Lab2;
 
-public class NumbersGUI {
-    public static void main(String[] args) {
-        int[] frameSize = {500,150};
-        appWindow mainframe  = new appWindow(
-                "Numbers Application",
-                frameSize,
-//                20,
-                "Please enter your numbers here",
-                "No numbers entered yet"
-                );
-        mainframe.inputField("Please enter your numbers here",20);
-        mainframe.prepareGUI();
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-        System.out.println(mainframe.getUserInputText());
+public class NumbersGUI {
+    static JFrame frame = new JFrame("Numbers GUI");
+    static JPanel panel = new JPanel();
+    static JTextField input = new JTextField(20);
+    static JLabel label = new JLabel("Please enter your numbers here");
+    static JLabel output = new JLabel("No numbers entered yet");
+    public static void main(String[] args) {
+
+        frame.setSize(500,100);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel.setLayout(new FlowLayout());
+
+        panel.add(label);
+        panel.add(input);
+        panel.add(output);
+
+
+        input.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (input.getText().equals("")) {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "You must enter something",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+                String userInputText = input.getText();
+
+                output.setText(userInputText);
+            }
+        });
+
+
+//        output.setText(userInputText);
+
+        frame.add(panel);
+
+        frame.setVisible(true);
     }
 }

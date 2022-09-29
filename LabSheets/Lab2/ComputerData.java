639d3d1eca4943eef0d7975d312c70e41166b18b
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-09-29 18:38:32 +0100

    220929 PERFECTION

--- LabSheets/Lab2/ComputerData.java
+++ LabSheets/Lab2/ComputerData.java
@@ -4,13 +4,36 @@ import javax.swing.*;
 import java.util.*;
 
 public class ComputerData {
+
+    private static boolean digitValidation(String bs){
+        boolean valid = false;
+        while (!valid) {
+            for (int i = 0; i < bs.length(); i++) {
+                if (!Character.isDigit(bs.charAt(i))) {
+                    bs = JOptionPane.showInputDialog("Invalid! Please re-enter the price: ");
+                }else valid = true;
+            }
+        }
+        return true;
+    }
+
+    private static boolean validRange(String target){
+        boolean valid = false;
+        int bs = Integer.parseInt(target);
+        while(!valid) {
+            if (bs < 50 || bs > 500) {
+                bs = Integer.parseInt(JOptionPane.showInputDialog("Invalid! Please re-enter the hard-disk space: "));
+            }else valid = true;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         try {
-            List<String> allSerial = new ArrayList<>();
-            List<Integer> allStorage = new ArrayList<>();
-            List<String> allCpu = new ArrayList<>();
-            List<Double> allPrice = new ArrayList<>();
             int iter = 0;
+            int storageAsInt = 0;
+            int allStorage = 0;
+            double minPrice = Double.MAX_VALUE , maxPrice = Double.MIN_VALUE;
+            String cheapestCPU = "";
 
             do {
                 String serial;
@@ -19,42 +42,60 @@ public class ComputerData {
                 String price;
 
                 serial = JOptionPane.showInputDialog("Please enter the serial number (<CR> to exit): ");
-                if (Objects.equals(serial.toLowerCase(Locale.ROOT), "cr")){
-                    JOptionPane.showMessageDialog(null, "Exited Program") ;
+                if (Objects.equals(serial.toLowerCase(Locale.ROOT), "cr")) {
+                    JOptionPane.showMessageDialog(null, "Exited Program");
                     break;
-                }else {
-                    allSerial.add(serial);
                 }
 
                 storage = JOptionPane.showInputDialog("Please enter the hard-disk space: ");
-
                 boolean valid = false;
                 while (!valid) {
-                    for (int i = 0; i < storage.length(); i++) {
+                    int i;
+                    for (i = 0; i < storage.length(); i++) {
                         if (!Character.isDigit(storage.charAt(i))) {
-                            valid = true;
                             break;
                         }
                     }
-                    storage = JOptionPane.showInputDialog("Invalid! Please re-enter the hard-disk space: ");
+                    if(i < storage.length())
+                        storage = JOptionPane.showInputDialog(
+                                "Invalid! Please re-enter hard-disk space");
+                    else
+                    {
+                        storageAsInt = Integer.parseInt(storage);
 
+                        if (storageAsInt > 50 && storageAsInt < 5000)
+                            valid = true;
+                        else
+                            storage = JOptionPane.showInputDialog(
+                                    "Invalid! Please re-enter hard-disk space");
+                    }
                 }
-                allStorage.add(Integer.parseInt(storage));
+                allStorage+= storageAsInt;
 
                 cpu = JOptionPane.showInputDialog("Please enter the processor type: ");
-                allCpu.add(cpu);
 
-                price = JOptionPane.showInputDialog("Please enter the price");
-                allPrice.add(Double.parseDouble(price));
+                price = JOptionPane.showInputDialog("Please enter the price: ");
+                double currPrice = 0;
+                if(digitValidation(price)) {
+                    currPrice = Double.parseDouble(price);
+                }
+                if (currPrice > maxPrice) maxPrice = currPrice;
+                if (currPrice < minPrice) {
+                    minPrice = currPrice;
+                    cheapestCPU = cpu;
+                }
 
                 iter++;
             } while (true);
 
-            System.out.println(allSerial);
-            System.out.println(allStorage);
-            System.out.println(allCpu);
-            System.out.println(allPrice);
-            System.out.println(iter);
+            String outputStr = String.format(
+                        """
+                        The average disk space available on the computers processed is: %dGB
+                        The price range of the computers is from \u20ac%.2f to \u20ac%.2f
+                        The processor type on the cheapest computer is %s
+                        """,
+                    allStorage/iter,minPrice, maxPrice, cheapestCPU);
+            JOptionPane.showMessageDialog(null,outputStr,"Computer Stats", JOptionPane.INFORMATION_MESSAGE);
 
         } catch (Exception e) {
             System.out.println(e);

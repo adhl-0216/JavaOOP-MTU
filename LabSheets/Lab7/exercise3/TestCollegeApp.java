c9e8104a63768677c6019f95827b42da539d3830
T00229173 <Han.Lim.Oah@students.ittralee.ie>
2022-10-04 15:57:06 +0100

    221004

--- LabSheets/Lab7/exercise3/TestCollegeApp.java
+++ LabSheets/Lab7/exercise3/TestCollegeApp.java
@@ -1,6 +1,10 @@
 package Lab7.exercise3;
 
 public class TestCollegeApp {
+    static Department[] departments = new Department[5];
+    static Institute ITT = new Institute("Institute of Technology, Tralee", departments);
+
+
     public static void main(String[] args) {
         Student[] compStds = new Student[10];
 
@@ -18,7 +22,6 @@ public class TestCollegeApp {
         mediaStds[0] = m1;
         mediaStds[1] = m2;
 
-        Department[] departments = new Department[5];
 
         Department computing = new Department("Computing", compStds);
         Department creativeMedia = new Department("Creative Media", mediaStds);
@@ -26,9 +29,32 @@ public class TestCollegeApp {
         departments[0] = computing;
         departments[1] = creativeMedia;
 
-        Institute ITT = new Institute("Institute of Technology, Tralee", departments);
-
-//print output
         System.out.println(ITT);
+
+        changeDept(154345, "computing", "creativeMedia");
+//        changeDept(453726, computing, creativeMedia);
+    }
+
+    private static void changeDept(int stdID, String from, String to) {
+        boolean foundFrom = false, foundTo = false;
+        for (Department dept:ITT.getDepartments()) {
+            if (dept != null) {
+                if (dept.getName().equals(from)) foundFrom = true;
+                if (dept.getName().equals(to)) foundTo  = true;
+//                System.out.println("Valid Departments.");
+            }
+        }
+
+        if (foundFrom&&foundTo){
+//                    System.out.println("here");
+            for (Student std: from) {
+                if (std != null) {
+                    if (std.getId() == stdID) {
+                        System.out.println("Found " + std.getName() + "!");
+                        std.setDepartment(to);
+                    }
+                }
+            }
+        }
     }
 }

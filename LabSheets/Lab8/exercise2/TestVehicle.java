68ba3d2dcb1b7d0ac01f8f0dd9cb00fefefdb63c
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-10-05 16:45:44 +0100

    221005 PERFECTION

--- LabSheets/Lab8/exercise2/TestVehicle.java
+++ LabSheets/Lab8/exercise2/TestVehicle.java
@@ -1,14 +1,13 @@
 package Lab8.exercise2;
 
-import javax.swing.*;
-
 public class TestVehicle {
     public static void main(String[] args) {
-/*        Car c1 =  new Car();
+
+        System.out.println("Testing the Car no-arg constructor: \n");
+        Car c1 =  new Car();
         System.out.println(c1+"\n\n");
-        Bicycle b1 =  new Bicycle();
-        System.out.println(b1+"\n\n");*/
-        String reg = JOptionPane.showInputDialog("reg");
+
+        System.out.println("Testing the Car multi-arg constructor: \n");
         Car c2 =  new Car(
                 10000,
                 4.5,
@@ -17,9 +16,24 @@ public class TestVehicle {
                 "Ferrari",
                 "F2",
                 2,
-                reg);
+                "08-KY-1234");
         System.out.println(c2+"\n\n");
-/*        Bicycle b2 =  new Bicycle(500,1.5,1.0,50, "Raleigh", "Mountainbike", 4, true);
-        System.out.println(b2);*/
+
+        System.out.println("Testing the Bicycle no-arg constructor: \n");
+
+        Bicycle b1 =  new Bicycle();
+        System.out.println(b1+"\n\n");
+
+        System.out.println("Testing the Bicycle multi-arg constructor: \n");
+        Bicycle b2 =  new Bicycle(
+                500,
+                1.5,
+                1.0,
+                50,
+                "Raleigh",
+                "Mountainbike",
+                4,
+                true);
+        System.out.println(b2);
     }
 }
